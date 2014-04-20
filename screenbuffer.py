304f7126ecc9e588675dc910fdbea7824d62e0b6
Kenny Levinsen <kennylevinsen@gmail.com>
2014-04-21 01:10:34 +0200

    Progress towards cleaner mode handling, as well as mode optimizing

--- screenbuffer.py
+++ screenbuffer.py
@@ -1,12 +1,9 @@
-
 class ScreenBuffer(object):
     '''Screen buffer
 
     Use of this screen buffer allows for optimized differential rendition of the screen, given the previously rendered screen object.
 
-    Optimized rendition is enabled when compile is given a previous screen object, as it enables differential rendition. During this type of partial rendering, a lot of cursor moves can occur. This is optimized by calculating the tradeoff of issuing a cursor-move, compared to just rendering the characters in between. This makes this renderer very bandwidth efficient.
-
-    In the future, it will also enable optimizing graphics modes, to avoid issuing bogus mode set/reset commands.'''
+    Optimized rendition is enabled when compile is given a previous screen object, as it enables differential rendition. During this type of partial rendering, a lot of cursor moves can occur. This is optimized by calculating the tradeoff of issuing a cursor-move, compared to just rendering the characters in between. This makes this renderer very bandwidth efficient.'''
     def __init__(self, height, width):
         '''Initialize a screen buffer of height * width
 
@@ -14,23 +11,56 @@ class ScreenBuffer(object):
         :param int width: The width of the screen buffer'''
         self.height = height
         self.width = width
-        self.lines = [[' ' for y in range(width)] for i in range(height)]
+        self.lines = [[(' ', None, None) for y in range(width)] for i in range(height)]
+        self.prev_modes = (None, None)
 
-    def set(self, x, y, val):
+    def set(self, x, y, val, fg=None, bg=None):
         '''Set a cell in the screen buffer
 
         :param int x: The x coordinate
         :param int y: The y coordinate
-        :param str val: The value to set'''
-        self.lines[y][x] = val
+        :param str val: The value to set
+        :param str fg: The foreground mode
+        :param str bg: The background mode'''
+        self.lines[y][x] = (val, fg, bg)
 
     def get(self, x, y):
         '''Get a cell from the screen buffer
 
         :param int x: The x coordinate
         :param int y: The y coordinate
-        :returns: str -- The content of the cell'''
+        :returns: tuple -- The content of the cell'''
         return self.lines[y][x]
+
+    def debug(self):
+        p = []
+        for i,l in enumerate(self.lines):
+            m = self.modes[i]
+            s = [(l[x], m[x]) for x in range(len(m))]
+            p.append('%d: %s' % (i, s))
+        return '\n'.join(p)
+
+    def diff(self, old):
+        changed_coords = []
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                oc, of, ob = old.lines[y][x]
+                nc, nf, nb = self.lines[y][x]
+                if (oc, of, ob) != (nc, nf, nb):
+                    changed_coords.append((x, y))
+        return changed_coords
+
+    def compile_char(self, x, y, res):
+        c, f, b = self.lines[y][x]
+        of, ob = self.prev_modes
+        if f != of:
+            if f is None: res.append('\x1b[39m')
+            else: res.append(f)
+        if b != ob:
+            if b is None: res.append('\x1b[49m')
+            else: res.append(b)
+        self.prev_modes = (f, b)
+        res.append(c)
 
     def compile(self, old=None):
         '''Compile render-string
@@ -49,13 +79,8 @@ class ScreenBuffer(object):
             return self.compile()
 
         # Compile diff
-        changed_coords = []
-        for y in range(0, self.height):
-            for x in range(0, self.width):
-                old_char = old.lines[y][x]
-                new_char = self.lines[y][x]
-                if old_char != new_char:
-                    changed_coords.append((x, y))
+        changed_coords = self.diff(old)
+        self.prev_modes = old.prev_modes
 
         # We need to keep a track
         res = []
@@ -66,24 +91,27 @@ class ScreenBuffer(object):
             if origin_move and y == prev_y and x == prev_x + 1:
                 # Single-character move
                 # Cursor moves forward by itself, so no work necessary
-                res.append(self.lines[y][x])
+                sefl.compile_char(x, y, res)
             elif origin_move and y == prev_y and x < prev_x + 6:
                 # Multi-character move
                 # Short enough that it's faster to just render
                 # everything up to this character to move the cursor
-                res.append(''.join(self.lines[y][prev_x+1:x+1]))
+                for i in range(prev_x+1, x+1):
+                    self.compile_char(i, y, res)
             else:
                 # Long multi-character move, or first move
                 # Use a cursor move
-                c = self.lines[y][x]
-                res.append('\x1b[%d;%dH%s' % (y+1, x+1, c))
+                res.append('\x1b[%d;%dH' % (y+1, x+1))
+                self.compile_char(x, y, res)
                 origin_move = True
         return ''.join(res)
 
     def compile_full(self):
         '''Compiles a regular render-string
         :returns: str - The rendered command string'''
-        s = []
-        for i,line in enumerate(self.lines):
-            s.append('\x1b[%d;1H%s' % (i+1, ''.join(line)))
-        return ''.join(s)
+        res = []
+        for y in range(self.height):
+            res.append('\x1b[0m\x1b[%d;1H' % (y + 1))
+            for x in range(self.width):
+                self.compile_char(x, y, res)
+        return ''.join(res)
