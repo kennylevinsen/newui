d6b5f8c00be0a9245ef9a1dd73b6a72b30ebb68d
Kenny Levinsen <kennylevinsen@gmail.com>
2014-04-21 21:31:20 +0200

    Fix a small bug, slight cleanup and documentation

--- screenbuffer.py
+++ screenbuffer.py
@@ -9,10 +9,10 @@ class ScreenBuffer(object):
 
         :param int height: The height of the screen buffer
         :param int width: The width of the screen buffer'''
-        self.height = height
-        self.width = width
-        self.lines = [[(' ', None, None) for y in range(width)] for i in range(height)]
-        self.prev_modes = (None, None)
+        self._height = height
+        self._width = width
+        self._lines = [[(' ', None, None) for y in range(width)] for i in range(height)]
+        self._prev_modes = (None, None)
 
     def set(self, x, y, val, fg=None, bg=None):
         '''Set a cell in the screen buffer
@@ -22,7 +22,7 @@ class ScreenBuffer(object):
         :param str val: The value to set
         :param str fg: The foreground mode
         :param str bg: The background mode'''
-        self.lines[y][x] = (val, fg, bg)
+        self._lines[y][x] = (val, fg, bg)
 
     def get(self, x, y):
         '''Get a cell from the screen buffer
@@ -30,36 +30,38 @@ class ScreenBuffer(object):
         :param int x: The x coordinate
         :param int y: The y coordinate
         :returns: tuple -- The content of the cell'''
-        return self.lines[y][x]
+        return self._lines[y][x]
 
-    def debug(self):
-        p = []
-        for i,l in enumerate(self.lines):
-            m = self.modes[i]
-            s = [(l[x], m[x]) for x in range(len(m))]
-            p.append('%d: %s' % (i, s))
-        return '\n'.join(p)
+    def _diff(self, old):
+        '''Internal diff calculator between two ScreenBuffers.
 
-    def diff(self, old):
+        :param ScreenBuffer old: The old ScreenBuffer
+        :returns: list -- List of (x, y) tuples of all the changes'''
         changed_coords = []
-        for y in range(0, self.height):
-            for x in range(0, self.width):
-                oc, of, ob = old.lines[y][x]
-                nc, nf, nb = self.lines[y][x]
+        for y in range(0, self._height):
+            for x in range(0, self._width):
+                oc, of, ob = old._lines[y][x]
+                nc, nf, nb = self._lines[y][x]
                 if (oc, of, ob) != (nc, nf, nb):
                     changed_coords.append((x, y))
         return changed_coords
 
-    def compile_char(self, x, y, res):
-        c, f, b = self.lines[y][x]
-        of, ob = self.prev_modes
+    def _compile_char(self, x, y, res):
+        '''Internal handler for rendering a character with a mode.
+        The character is read from internal list of lines.
+
+        :param int x: The x coordinate
+        :param int y: The y coordinate
+        :param list res: The result list'''
+        c, f, b = self._lines[y][x]
+        of, ob = self._prev_modes
         if f != of:
             if f is None: res.append('\x1b[39m')
             else: res.append(f)
         if b != ob:
             if b is None: res.append('\x1b[49m')
             else: res.append(b)
-        self.prev_modes = (f, b)
+        self._prev_modes = (f, b)
         res.append(c)
 
     def compile(self, old=None):
@@ -75,34 +77,34 @@ class ScreenBuffer(object):
             return self.compile_full()
 
         # Do not attempt optimized rendition after rescale
-        if self.height != old.height or self.width != old.width:
+        if self._height != old._height or self._width != old._width:
             return self.compile()
 
         # Compile diff
-        changed_coords = self.diff(old)
-        self.prev_modes = old.prev_modes
+        changed_coords = self._diff(old)
+        self._prev_modes = old._prev_modes
 
         # We need to keep a track
         res = []
         origin_move = False
-        prev_x = -1
-        prev_y = -1
+        prev_x = -10
+        prev_y = -10
         for x,y in changed_coords:
             if origin_move and y == prev_y and x == prev_x + 1:
                 # Single-character move
                 # Cursor moves forward by itself, so no work necessary
-                sefl.compile_char(x, y, res)
+                self._compile_char(x, y, res)
             elif origin_move and y == prev_y and x < prev_x + 6:
                 # Multi-character move
                 # Short enough that it's faster to just render
                 # everything up to this character to move the cursor
                 for i in range(prev_x+1, x+1):
-                    self.compile_char(i, y, res)
+                    self._compile_char(i, y, res)
             else:
                 # Long multi-character move, or first move
                 # Use a cursor move
                 res.append('\x1b[%d;%dH' % (y+1, x+1))
-                self.compile_char(x, y, res)
+                self._compile_char(x, y, res)
                 origin_move = True
         return ''.join(res)
 
@@ -110,8 +112,8 @@ class ScreenBuffer(object):
         '''Compiles a regular render-string
         :returns: str - The rendered command string'''
         res = []
-        for y in range(self.height):
+        for y in range(self._height):
             res.append('\x1b[0m\x1b[%d;1H' % (y + 1))
-            for x in range(self.width):
-                self.compile_char(x, y, res)
+            for x in range(self._width):
+                self._compile_char(x, y, res)
         return ''.join(res)
